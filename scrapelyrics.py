40cc6702e265c303323896873a6356d8512df8e3
Peter <loomispeter@gmail.com>
2014-10-28 23:41:02 -0400

    generating verses

--- scrapelyrics.py
+++ scrapelyrics.py
@@ -2,7 +2,11 @@
 """
 
 from lxml import html
-import requests, argparse
+from collections import defaultdict
+import requests, argparse, string, random
+
+START_TAG = "<s>"
+VERSE_LENGTH = 10
 
 def compile_corpus_for_genre(genre):
     """
@@ -18,21 +22,63 @@ def compile_corpus_for_genre(genre):
         song_tree = html.fromstring(lyric_page.text)
         verses = song_tree.xpath('//*[@class="verse"]/text()')
         for verse in verses:
-            f.write(verse.encode('utf-8'))
+            f.write(verse.encode('utf-8')+'\n')
     f.close()
 
 def create_ngram_model(filename):
     """
-    Accumulate trigram and unigram counts using a corpus of lyrics from a certain genre of music
+    Accumulate trigram, bigram, and unigram counts using a corpus of lyrics from a certain genre of music
+    Returns a dictionary containing the ngram counts collected from the corpus file
     """
-    print filename
+    countsdict = defaultdict(float)
+    for line in open(filename):
+        words = line.split()
+        for i in range(len(words)):
+            # unigrams
+            countsdict[words[i].strip()]+=1.0
+            # bigrams
+            if i == 0:
+                countsdict[START_TAG+" "+words[i].strip()]+=1.0
+            elif i >= 1:
+                countsdict[words[i-1].strip()+" "+words[i].strip()]+=1.0
+            # trigrams
+            if i >= 2:
+                countsdict[words[i-2].strip()+" "+words[i-1].strip()+" "+words[i].strip()]+=1.0
+    return countsdict
+
+def generate_lyrics(model):
+    """
+    Use an ngram model to generate lines of lyrics for a certain genre of music
+    """
+    # Choose a random word to start the lyric. Choose from the set of words that follow a start tag.
+    start = random.choice([ngram for ngram in model.keys() if START_TAG in ngram.split()]).split()[1]
+    unigrams = [ngram for ngram in model.keys() if len(ngram.split()) == 1]
+    sequence = start
+    for i in range(1, VERSE_LENGTH):
+        # Choose the next word in the generated sequence based on bigram probabilities
+        nextword = ""
+        bestBigramProb = 0.0
+        for token in unigrams:
+            if sequence.split()[i-2]+" "+sequence.split()[i-1]+" "+token in model.keys():
+                prob = model[sequence.split()[i-2]+" "+sequence.split()[i-1]+" "+token]/model[sequence.split()[i-1]+" "+token]
+            elif sequence.split()[i-1]+" "+token in model.keys():
+                prob = model[sequence.split()[i-1]+" "+token]/model[sequence.split()[i-1]]
+            else:
+                prob = model[token]/sum(model.values())
+            
+            if prob > bestBigramProb:
+                bestBigramProb = prob
+                nextword = token
+        sequence = sequence+" "+nextword
+    print sequence
 
 if __name__=='__main__':
-    genres = ['rock','hiphop','pop']
-    for g in genres:
-        compile_corpus_for_genre(g)
+    #genres = ['rock','hiphop','pop']
+    #for g in genres:
+    #    compile_corpus_for_genre(g)
     parser = argparse.ArgumentParser()
     parser.add_argument('-generate', type = str, required = False, choices = ['rock','hiphop','pop'], help = 'generate genre lyrics (genre)')
     args = parser.parse_args()
     if args.generate:
-        create_ngram_model(args.generate+'.txt')
+        model = create_ngram_model(args.generate+'.txt')
+        generate_lyrics(model)
