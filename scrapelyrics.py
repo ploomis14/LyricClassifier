3d2644f0d95fbd92ad2a5aaed445ae946532d405
Peter <loomispeter@gmail.com>
2014-11-16 22:16:15 -0500

    refactored classifier

--- scrapelyrics.py
+++ scrapelyrics.py
@@ -26,13 +26,13 @@ VERSES_PER_SONG = 3
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
-def compile_corpus_for_genre(genre):
+def compile_corpus_for_genre(genre,filename,npages):
     """
     Scrapes website for song lyrics and compiles corpora with lyrics from each genre of music.
     Compile a corpus of lyrics for a certain genre of music
     """
-    f = open(genre+'.txt','w')
-    for i in range(CORPUS_SIZE):
+    f = open(filename,'w')
+    for i in range(npages):
         page = requests.get('http://genius.com/tags/'+genre+'/all?page='+str(i))
         tree = html.fromstring(page.text)
         songs = tree.xpath('//*[@class=" song_link"]/@href')
@@ -94,14 +94,40 @@ def create_ngram_model(filename):
 
     return countsdict
 
+def create_train_data(genres):
+    """
+    Make training files for each genre from the first two pages of song data
+    """
+    for genre in genres:
+        compile_corpus_for_genre(genre,genre+"train.txt",2)
+
+def create_test_data(genre):
+    """
+    Make testing files for each genre from one page of songs
+    """
+    i = 0
+    page = requests.get('http://genius.com/tags/'+genre+'/all?page=3')
+    tree = html.fromstring(page.text)
+    songs = tree.xpath('//*[@class=" song_link"]/@href')
+    for song in songs:
+        f = open(genre + "test%s.txt" % str(i), 'w')
+        lyric_page = requests.get(song)
+        song_tree = html.fromstring(lyric_page.text)
+        verses = song_tree.xpath('//*[@data-editorial-state="accepted"]/text()')
+        for verse in verses:
+            if verse.strip("[]") == verse:  #skip annotations like "[Chorus 1]" and "[Sample: <song>]"
+                f.write(verse+'\n')
+        i+=1
+        f.close()
+
 class LyricGenerator:
     def __init__(self, genre):
         """
         Collects corpus of lyrics, constructs ngram model for genre
         """
-        if not os.path.exists(genre+'.txt') or os.path.getsize(genre+'.txt') == 0:
-            compile_corpus_for_genre(genre)
         filename = genre+'.txt'
+        if not os.path.exists(filename) or os.path.getsize(filename) == 0:
+            compile_corpus_for_genre(genre,filename,CORPUS_SIZE)
         self.model = create_ngram_model(filename)
 
     def approx_nsyl(self,word):
@@ -199,39 +225,36 @@ class LyricGenerator:
         output_file.write(lyrics)
         output_file.close()
 
-"""
+
 class LyricClassifier:
-    def __init__():
+    def __init__(self):
+        """
+        Constructs models from training data, gets testing data by scraping songs on the website and
+        outputing each to a file
+        """
+        self.genre_list = ['rock', 'rap']
+        create_train_data(self.genre_list)
 
+        rock = create_ngram_model('rocktrain.txt')
+        rap = create_ngram_model('raptrain.txt')
 
-    def create_test_data(genre, page):
-        i = 0
-        page = requests.get('http://genius.com/tags/'+genre+'/all?page='+str(page))
-        tree = html.fromstring(page.text)
-        songs = tree.xpath('//*[@class=" song_link"]/@href')
-        for song in songs:
-            f = open(genre + "test%s.txt" % str(i), 'w')
-            lyric_page = requests.get(song)
-            song_tree = html.fromstring(lyric_page.text)
-            verses = song_tree.xpath('//*[@data-editorial-state="accepted"]/text()')
-            for verse in verses:
-                if verse.strip("[]") == verse:  #skip annotations like "[Chorus 1]" and "[Sample: <song>]"
-                    f.write(verse.encode('utf-8')+'\n')
-            i+=1
-            f.close()
+        for genre in self.genre_list:
+            create_test_data(genre)
+        
+        self.genre_model_list = [rock, rap]
 
-    def generate_key(seq):
+    def generate_key(self, seq):
         key = ""
         for item in seq:
             key += item + " "
         
         return key
 
-    def classify(models, filename):
+    def classify(self, filename):
         maxprob = float('-inf')
         best_fit = {}
         #find p(lyrics) for each model
-        for model in models:
+        for model in self.genre_model_list:
             #print model
             totalprob = 0.0
             backpointers = ['<s>','<s>','<s>']
@@ -253,7 +276,7 @@ class LyricClassifier:
                     j = 0
                     l = .9
                     while j < len(backpointers):
-                        key = generate_key(backpointers[j:len(backpointers)])
+                        key = self.generate_key(backpointers[j:len(backpointers)])
                         #print key
                         prob += l*model[key.strip()]
                         #print key
@@ -280,12 +303,13 @@ class LyricClassifier:
         
         if best_fit == {}:
             return 0
-        return models.index(best_fit)"""
+        index = self.genre_model_list.index(best_fit)
+        return self.genre_list[index]
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-generate', type = str, required = False, choices = ['rock','rap','pop'], help = 'generate genre lyrics (rock, rap, pop)')
-    parser.add_argument('-classify', type = str, required = False,  help = 'classify the genre of an unlabeled set of lyrics')
+    parser.add_argument('-classify', help = 'classify the genre of an unlabeled set of lyrics')
     args = parser.parse_args()
     
     if args.generate:
@@ -294,22 +318,16 @@ if __name__=='__main__':
         generator.output_lyrics('generate-'+args.generate+'.txt')
     
     if args.classify:
-        ROCK = create_ngram_model('rocktrain.txt')
-        RAP = create_ngram_model('raptrain.txt')
-        POP = create_ngram_model('poptrain.txt')
-        
-        genre_list = ['Rock', 'Rap', 'Pop']
-        genre_model_list = [ROCK, RAP, POP]
-        
+        genre_list = ['rap','rock']
+        classifier = LyricClassifier()
         errors = 0.0
         total = 0.0
         for item in genre_list:
             for n in range(20):
-                guess = genre_list[classify(genre_model_list, item.lower() + 'test%s.txt' % n)]
-                print item, guess
+                guess = classifier.classify(item.lower() + 'test%s.txt' % n)
+                print "guessed "+guess+" for "+item+" song"
                 if item != guess:
                     errors += 1
                 total += 1
 
-        print "Accuracy: " + str((total-errors)/total)
-#print "Lyrics classified as " + genre_list[classify(genre_model_list, args.classify)] + "."
+        print "Accuracy: " + str(100*((total-errors)/total))
